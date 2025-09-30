ef058d199ca5dc140521ba2d092301d7ce71483a
BaseDeltaZer0 <AndyDDennis@comcast.net>
2025-09-30 04:52:14 -0400

    Created basic text output.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+def get_book_text(file_path): #Takes a file path as input...
+    with open(file_path, "r", encoding="utf-8") as f: #Opens it...
+        contents = f.read() #Returns the contents as a string.
+        return (contents)
+
+main()
