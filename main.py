7250e5b32a5e1c749ac89da4e5bd5f195d07fb3c
BaseDeltaZer0 <AndyDDennis@comcast.net>
2025-09-30 18:07:16 -0400

    Modified to print word count.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    content = (get_book_text("books/frankenstein.txt"))
+    num_words = get_word_count(content)
+    print(f"Found {num_words} total words")
+    
 
 def get_book_text(file_path): #Takes a file path as input...
     with open(file_path, "r", encoding="utf-8") as f: #Opens it...
-        contents = f.read() #Returns the contents as a string.
-        return (contents)
+        content = f.read() #Returns the contents as a string.
+        return (content)
+
+def get_word_count(content):
+    word_list = content.split()
+    word_count = len(word_list)
+    return word_count
 
 main()
